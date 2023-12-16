0bb232b08f15eade70e0c4d5d1838182c0e35082
kooigol123 <tritontriton008@gmail.com>
2023-12-16 21:23:48 +0700

    Change folder

--- Script.js
+++ Script.js
@@ -1,3 +1,19 @@
+function Menu() {
+      document.getElementById("Mydropdown").classList.toggle("show");
+      window.onclick = function (event) {
+            if (!event.target.matches('.bar')) {
+                  var dropdowns = document.getElementsByClassName("dropdown-bar");
+                  var i;
+                  for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                              openDropdown.classList.remove('show');
+                        }
+                  }
+            }
+      }
+}
+
 let Gmail = 'tritontriton008@gmail.com';
 function Copygmail() {
       navigator.clipboard.writeText(Gmail);
@@ -6,12 +22,11 @@ function Copygmail() {
       } else {
             document.getElementById("Copygmail").style.display = "none";
             document.getElementById("Copyedgmail").style.display = "block";
-            document.getElementById("tooltiptext-gmail").style.display = "none";
-            
-      }
-      
-      
-}
+
+      }      
+
+
+}      
 let Phone = '0621605262';
 function Copyphone() {
       navigator.clipboard.writeText(Phone);
@@ -20,24 +35,7 @@ function Copyphone() {
       } else {
             document.getElementById("Copyphone").style.display = "none";
             document.getElementById("Copyedphone").style.display = "block";
-            document.getElementById("tooltiptext-phone").style.display = "none";
-      } 
-     
-}
+      }      
 
-function Menu() {
-      document.getElementById("Mydropdown").classList.toggle("show");
-      window.onclick = function(event) {
-                              if (!event.target.matches('.bar')) {
-                              var dropdowns = document.getElementsByClassName("dropdown-bar");
-                              var i;
-                              for (i = 0; i < dropdowns.length; i++) {
-                                    var openDropdown = dropdowns[i];
-                                    if (openDropdown.classList.contains('show')) {
-                                          openDropdown.classList.remove('show');
-                            }
-                           }
-                          }
-                         }
-      }
-      
+}      
+
